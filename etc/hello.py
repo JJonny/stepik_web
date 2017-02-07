50a685be266f5abd53bf921da5d17e900125e8fd
JJonny <qweer.baranovsky@gmail.com>
2017-02-07 14:21:33 +0300

    added files

---
+++ etc/hello.py
@@ -0,0 +1,15 @@
+from cgi import parse_qs
+
+def app(environ, start_response):
+    qs = parse_qs(environ['QUERY_STRING'])
+    res = ['%s=%s\n' % (k, qs[k][0]) for k in qs]
+
+    # res = []
+    # for k, qs in environ.items():
+    #     if 'QUERY_STRING' in k:
+    #         for i in qs.split('&'):
+    #             res.append(str.encode(i + '\n'))
+
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+
+    return res
