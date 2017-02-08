c301b463a20fe66cc52251b7f287f51e8b641ec8
JJonny <qweer.baranovsky@gmail.com>
2017-02-08 21:17:36 +0300

    update pathes init.sh

--- etc/init.sh
+++ etc/init.sh
@@ -1,6 +1,6 @@
 sudo rm -rf /etc/nginx/sites-enabled/default
-sudo ln -sf ~/stepik_web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -sf ~/stepik_web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
 sudo gunicorn -b 0.0.0.0:8080 hello:app
