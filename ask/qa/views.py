9cf5b5ff91ce820d54fbe387ecf6af980f513389
JJonny <qweer.baranovsky@gmail.com>
2017-06-14 13:34:35 +0300

    Optimized code
    Optimized listing and popular views. Cleaned urls. More beter looks Like and Answer counters. Llogin, logout and signup shifted to the right.

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -35,19 +35,21 @@ def popular(request):
     questions = Question.objects.popular()
     paginator = Paginator(questions, 10)
     page = request.GET.get('page')
-    try:
-        page_posts = paginator.page(page)
-    except PageNotAnInteger:
-        page_posts = paginator.page(page)
-    except EmptyPage:
-        page_posts = paginator.page(paginator.num_pages)
-    return render(request, 'blog/popular.html', {'page_posts': page_posts})
+    return render(request, 'blog/home.html',
+                            {'page_posts': pagination_page(paginator, page),
+                             'user': request.user, })
 
 
 def listing(request):
     post_all = Question.objects.new()
     paginator = Paginator(post_all, 10)
     page = request.GET.get('page')
+    return render_to_response('blog/home.html',
+                              {'page_posts': pagination_page(paginator, page),
+                               'user': request.user, })
+
+
+def pagination_page(paginator, page):
     try:
         page_posts = paginator.page(page)
     except PageNotAnInteger:
@@ -55,10 +57,7 @@ def listing(request):
         page_posts = paginator.page(page)
     except EmptyPage:
         page_posts = paginator.page(paginator.num_pages)
-    return render_to_response('blog/home.html',
-                              {'page_posts': page_posts,
-                               'user': request.user,
-                               'session': request.session, })
+    return page_posts
 
 
 def about(request):
