8df22c2fc8bb318615ef988230d69be58e38333e
JJonny <qweer.baranovsky@gmail.com>
2017-06-14 13:34:35 +0300

    Optimized code
    Optimized listing and popular views. Cleaned urls. More beter looks Like and Answer counters. Llogin, logout and signup shifted to the right. Change Fonts.

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import url
 from . import views
 
 app_name = 'qa'
-
 urlpatterns = [
     url(r'^$', views.listing, name='listing'),
     url(r'^home/', views.listing, name='listing'),
@@ -11,9 +10,9 @@ urlpatterns = [
     url(r'^question/(?P<id>\d+/?)/', views.question, name='question'),
     url(r'^ask/$', views.new_ask, name='ask'),
     url(r'^popular/', views.popular, name='popular'),
-    url(r'^new/', views.test, name='new'),
     url(r'^auth_logout/', views.auth_logout, name='auth_logout'),
     url(r'^about/', views.about, name='about'),
     url(r'^ajax/validate_username/$', views.validate_username, name='validate_username'),
     url(r'^ajax/rating_up/$', views.rating_up, name='rating_up'),
 ]
+
