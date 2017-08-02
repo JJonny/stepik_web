0e0d34b9b7459251393b084a0452529b43abb0d7
JJonny <qweer.baranovsky@gmail.com>
2017-08-02 10:27:34 +0300

    Add search fild
    In admin page add search fild.

--- ask/qa/admin.py
+++ ask/qa/admin.py
@@ -2,6 +2,13 @@ from django.contrib import admin
 from qa.models import Question, Answer
 
 
-# Register your models here.
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Question._meta.fields]
+    search_fields = ['id']
+
+    class Meta:
+        model = Question
+
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Answer)
